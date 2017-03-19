35a40802436ace5dac07945a8970a8098dcd0ef0
1070Alienware <velaboratory@gmail.com>
2017-03-19 18:02:50 -0400

    Colliders are working now
    Still have to work on the GUI so it shows canvas nicely in VR world.

---
+++ Assets/Scripts/drumCollider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drumCollider : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        //Debug.Log("Hit something");
+
+       // if (c.name == "Snare")
+        {
+
+           Debug.Log(gameObject.name + " is hitting " + c.name);
+
+        }
+    }
+}
